8952eb5b73e87ccc763e1efe903da055aeb74709
DESKTOP-GDRF1VF\Chompis <jmontero048@yahoo.es>
2024-07-08 14:27:34 -0600

    hoy

--- Practica/Practica.Client/obj/Debug/net8.0/Practica.Client.AssemblyInfo.cs
+++ Practica/Practica.Client/obj/Debug/net8.0/Practica.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9da4c869ea5d7933fb6914b35bc6c7f8e64cdd56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Practica/Practica/obj/Debug/net8.0/Practica.AssemblyInfo.cs
+++ Practica/Practica/obj/Debug/net8.0/Practica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9da4c869ea5d7933fb6914b35bc6c7f8e64cdd56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
